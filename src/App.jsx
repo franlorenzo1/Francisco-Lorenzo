5f35093cbbef0cf4538b107596973dea833a31cb
franlorenzo1 <franciscopacolorenzo@gmail.com>
2026-06-08 13:28:57 -0300

    Fix: remover importacion de componente Contact eliminado

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import Hero from "./components/Hero";
 import About from "./components/About";
 import Skills from "./components/Skills";
 import Projects from "./components/Projects";
-import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 
 export default function App() {
@@ -22,9 +21,6 @@ export default function App() {
         </section>
         <section id="projects">
           <Projects />
-        </section>
-        <section id="contact">
-          <Contact />
         </section>
       </main>
       <Footer />
